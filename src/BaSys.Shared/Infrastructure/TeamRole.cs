595b84fed6db8dc9ee53ecc691b4318411f3dbb3
ZyuDev <MikhaZyu@gmail.com>
2024-02-26 17:44:35 +0300

    Add Team role

---
+++ src/BaSys.Shared/Infrastructure/TeamRole.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace BaSys.Common.Infrastructure
+{
+    /// <summary>
+    /// Stores names of asp.net roles, used in the application.
+    /// </summary>
+    public sealed class TeamRole
+    {
+        public const string SuperAdministrator = "sa";
+        public const string Administrator = "administrator";
+        public const string Designer = "designer";
+        public const string User = "user";
+        public const string Readonly = "readonly";
+
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public TeamRole()
+        {
+
+        }
+
+        public TeamRole(string name, string title)
+        {
+            Name = name;
+            Title = title;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+
+    }
+}
