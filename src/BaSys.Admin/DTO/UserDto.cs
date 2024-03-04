efc32691ac8653c8dd09ca2ad9e80208a7dd4d45
ZyuDev <MikhaZyu@gmail.com>
2024-03-04 14:27:46 +0300

    #016-feature: get all users

---
+++ src/BaSys.Admin/DTO/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BaSys.Admin.DTO
+{
+    public sealed class UserDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
