b8d52016d95f764570fba46067a2dcfd59c2742d
ZyuDev <MikhaZyu@gmail.com>
2024-03-07 11:05:48 +0300

    #022-feature: Add Info to ResultWrapper

--- src/BaSys.Shared/Infrastructure/ResultWrapper.cs
+++ src/BaSys.Shared/Infrastructure/ResultWrapper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using static System.Net.WebRequestMethods;
 
 namespace BaSys.Common.Infrastructure
 {
@@ -17,21 +18,42 @@ namespace BaSys.Common.Infrastructure
         private const string SuccessMessage = "OK";
         private const string UnknownErrorMessage = "Unknown error";
 
+        /// <summary>
+        ///The status code.Note that this is NOT an HTTP status code, but an application-specific status code.A status of 0 indicates no errors.
+        /// </summary>
         public int Status { get; set; } = -1;
+        /// <summary>
+        /// The message intended for user display.
+        /// </summary>
         public string Message { get; set; } = UnknownErrorMessage;
+        /// <summary>
+        /// Additional technical information. This should not be displayed to the user but printed to the console for debugging purposes.
+        /// </summary>
+        public string Info { get; set; } = string.Empty;
+        /// <summary>
+        /// The result data.
+        /// </summary>
         public T Data { get; set; } = default(T);
+        /// <summary>
+        /// Indicates the absence of errors.
+        /// </summary>
         public bool IsOK => Status == 0;
+        /// <summary>
+        /// Combines all information into a single string.
+        /// </summary>
+        public string Presentation => $"{Status}:{Message}:{Info}";
 
         public ResultWrapper()
         {
             
         }
 
-        public ResultWrapper(T data, int status = -1, string message = null)
+        public ResultWrapper(T data, int status = -1, string message = null, string info = null)
         {
             Status = status;
             Data = data;
             Message = message ?? UnknownErrorMessage;
+            Info = info ?? string.Empty;
         }
 
         public void Success(T data)
@@ -40,22 +62,24 @@ namespace BaSys.Common.Infrastructure
             Message = SuccessMessage;
             Status = 0;
         }
-        public void Error(int status, string message)
+        public void Error(int status, string message, string info = null)
         {
             Data = default(T);
             Status = status;
             Message = message;
+            Info = info ?? string.Empty;
         }
 
-        public void SetStatus(int status, string message)
+        public void SetStatus(int status, string message, string info = null)
         {
             Status = status;
             Message = message;
+            Info = info ?? string.Empty;
         }
 
         public override string ToString()
         {
-            return $"{Status}:{Message}";
+            return Presentation;
         }
  
     }
