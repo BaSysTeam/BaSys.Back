2eacf792b9fa35a5eacf17cacb974e0d481b72a9
ZyuDev <MikhaZyu@gmail.com>
2024-03-09 12:07:45 +0300

    #023-feature: Add validation for collection Roles.

--- src/BaSys.Admin/DTO/UserValidatorBase.cs
+++ src/BaSys.Admin/DTO/UserValidatorBase.cs
@@ -8,6 +8,14 @@ namespace BaSys.Admin.DTO
         {
             RuleFor(x => x.UserName).NotEmpty().MaximumLength(UserDto.UserNameMaxLength);
             RuleFor(x => x.Email).NotEmpty().MaximumLength(UserDto.EmailMaxLength).EmailAddress();
+            RuleFor(x => x.Roles).Must(ContainUserWithIsCheckedTrue)
+                .WithMessage("User role is neccessary");
+        }
+
+        // This method checks if the collection meets the specific condition
+        private bool ContainUserWithIsCheckedTrue(IList<UserRoleDto> roles)
+        {
+            return roles.Any(r => r.Name.Equals("user", StringComparison.OrdinalIgnoreCase) && r.IsChecked);
         }
     }
 }
