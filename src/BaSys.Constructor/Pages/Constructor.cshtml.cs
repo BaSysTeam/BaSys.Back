844b559cd74e0c2c5f5f23654a57d70a7580098d
ZyuDev <MikhaZyu@gmail.com>
2024-03-13 13:37:41 +0300

    Build constructor vue app.

---
+++ src/BaSys.Constructor/Pages/Constructor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaSys.Constructor.Pages
+{
+    public class ConstructorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
