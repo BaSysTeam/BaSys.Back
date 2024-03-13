222b4186361f11aecdc447feadec6ba39db52545
Oceanshiver <62504973+Oceanshiver@users.noreply.github.com>
2024-03-13 16:30:19 +0300

    maindbcheckservice

--- src/BaSys.Host/Services/MainDbCheckService.cs
+++ src/BaSys.Host/Services/MainDbCheckService.cs
@@ -4,6 +4,7 @@ using BaSys.Host.Data;
 using BaSys.SuperAdmin.Data.Identity;
 using BaSys.SuperAdmin.Infrastructure.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace BaSys.Host.Services;
 
@@ -11,32 +12,73 @@ public class MainDbCheckService : IMainDbCheckService
 {
     private readonly UserManager<IdentityUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
-    private readonly ApplicationDbContext _context;
     
     public MainDbCheckService(UserManager<IdentityUser> userManager,
-        RoleManager<IdentityRole> roleManager,
-        ApplicationDbContext context)
+        RoleManager<IdentityRole> roleManager)
     {
         _userManager = userManager;
         _roleManager = roleManager;
-        _context = context;
     }
     
     public async Task Check(InitAppSettings initAppSettings)
     {
-        // Check roles
+        await CheckRoles();
+        var adminLogin = await CheckAdminUser(initAppSettings);
+        await CheckAdminRoles(adminLogin);
+    }
+
+    private async Task CheckAdminRoles(string? adminLogin)
+    {
+        if (!string.IsNullOrEmpty(adminLogin))
+        {
+            var adminUser = await _userManager.FindByEmailAsync(adminLogin);
+            if (adminUser == null)
+                return;
+            
+            var adminRoles = new[]
+            {
+                ApplicationRole.Administrator,
+                ApplicationRole.User,
+                ApplicationRole.Designer
+            };
+
+            foreach (var adminRole in adminRoles)
+            {
+                if (!await _userManager.IsInRoleAsync(adminUser, adminRole))
+                    await _userManager.AddToRoleAsync(adminUser, adminRole);
+            }
+        }
+    }
+
+    private async Task<string?> CheckAdminUser(InitAppSettings initAppSettings)
+    {
+        var isAdminSa = string.IsNullOrEmpty(initAppSettings.MainDb?.AdminLogin) ||
+                        string.IsNullOrEmpty(initAppSettings.MainDb?.AdminPassword);
+
+        var adminLogin = isAdminSa ? initAppSettings.Sa?.Login : initAppSettings.MainDb?.AdminLogin;
+        var adminPassword = isAdminSa ? initAppSettings.Sa?.Password : initAppSettings.MainDb?.AdminPassword;
+        
+        if (!string.IsNullOrEmpty(adminLogin) && !string.IsNullOrEmpty(adminPassword))
+        {
+            if (!await _userManager.Users.AnyAsync(x => x.NormalizedEmail == adminLogin.ToUpper()))
+            {
+                await _userManager.CreateAsync(new IdentityUser()
+                {
+                    UserName = adminLogin,
+                    Email = adminLogin
+                }, adminPassword);
+            }
+        }
+
+        return adminLogin;
+    }
+
+    private async Task CheckRoles()
+    {
         foreach (var role in ApplicationRole.AllApplicationRoles())
         {
             if (!await _roleManager.RoleExistsAsync(role.Title))
                 await _roleManager.CreateAsync(new SaDbRole(role.Title));
         }
-        
-        // Check admin
-        var isAdminSa = string.IsNullOrEmpty(initAppSettings.MainDb?.AdminLogin) ||
-                        string.IsNullOrEmpty(initAppSettings.MainDb?.AdminPassword);
-
-
-        // Check admin roles
-        // ...
     }
 }
