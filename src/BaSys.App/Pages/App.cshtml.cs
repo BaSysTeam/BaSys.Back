f7903b826f6c69f6be61440a65404bdd0be6bace
ZyuDev <MikhaZyu@gmail.com>
2024-03-13 14:01:34 +0300

    #043-feature: Create app project

---
+++ src/BaSys.App/Pages/App.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaSys.App.Pages
+{
+    public class AppModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
