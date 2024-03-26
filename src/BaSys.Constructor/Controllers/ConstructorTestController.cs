a80ab9ff54441b6405c35a4225cae8aa9aa8fa2f
ZyuDev <MikhaZyu@gmail.com>
2024-03-26 11:12:55 +0300

    #065-feature: Add constructor module.

---
+++ src/BaSys.Constructor/Controllers/ConstructorTestController.cs
@@ -0,0 +1,20 @@
+﻿using BaSys.Common.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaSys.Constructor.Controllers
+{
+    [Route("api/constructor/v1/[controller]")]
+    [ApiController]
+    public class ConstructorTestController : ControllerBase
+    {
+        [HttpGet("Ping")]
+        public IActionResult Ping()
+        {
+            var result = new ResultWrapper<DateTime>();
+            result.Success(DateTime.Now);
+
+            return Ok(result);  
+        }
+    }
+}
