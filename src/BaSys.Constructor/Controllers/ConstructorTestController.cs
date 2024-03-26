2ccce995c32f671a774fb8b0212fed4260fda40f
ZyuDev <MikhaZyu@gmail.com>
2024-03-26 12:22:19 +0300

    #065-feature: Add dbName Claim.

--- src/BaSys.Constructor/Controllers/ConstructorTestController.cs
+++ src/BaSys.Constructor/Controllers/ConstructorTestController.cs
@@ -1,6 +1,8 @@
 ﻿using BaSys.Common.Infrastructure;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace BaSys.Constructor.Controllers
 {
@@ -8,6 +10,13 @@ namespace BaSys.Constructor.Controllers
     [ApiController]
     public class ConstructorTestController : ControllerBase
     {
+        private IConfiguration _configuration;
+
+        public ConstructorTestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet("Ping")]
         public IActionResult Ping()
         {
@@ -15,6 +24,20 @@ namespace BaSys.Constructor.Controllers
             result.Success(DateTime.Now);
 
             return Ok(result);  
+        }
+
+        [HttpPost("MetadataGroupTable")]
+        public IActionResult CreateMetadataGroupTable()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var dbName = User.Claims.FirstOrDefault(c => c.Type == "DbName")?.Value;
+
+            var result = new ResultWrapper<string>();
+            result.Success(dbName);
+
+         
+
+            return Ok(result);
         }
     }
 }
