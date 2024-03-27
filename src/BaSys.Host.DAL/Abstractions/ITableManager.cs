8f6e266481d3db326487fe1e7cab0e24d10d7e71
ZyuDev <MikhaZyu@gmail.com>
2024-03-27 13:24:08 +0300

    #066-feature: Refactoring. Extract interface and abstract class for TableManager.

---
+++ src/BaSys.Host.DAL/Abstractions/ITableManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaSys.Host.DAL.Abstractions
+{
+    public interface ITableManager
+    {
+        string TableName { get; }
+        Task<int> CreateTableAsync(IDbTransaction transaction = null);
+        Task<int> DropTableAsync(IDbTransaction transaction = null);
+        Task<bool> TableExistsAsync(IDbTransaction transaction = null);
+    }
+}
