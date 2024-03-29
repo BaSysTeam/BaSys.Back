796bac6673786c26895dd56bf7de949d8e765eae
ZyuDev <MikhaZyu@gmail.com>
2024-03-29 18:17:27 +0300

    #072-feature: Update model and Update builder

---
+++ src/BaSys.FluentQueries/QueryBuilders/UpdateBuilder.cs
@@ -0,0 +1,73 @@
+﻿using BaSys.FluentQueries.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BaSys.FluentQueries.QueryBuilders
+{
+    public sealed class UpdateBuilder
+    {
+        private readonly UpdateModel _model;
+
+        public UpdateBuilder()
+        {
+            _model = new UpdateModel();
+        }
+
+        public UpdateBuilder(UpdateModel model)
+        {
+            _model = model;
+        }
+
+        public UpdateBuilder Table(string tableName)
+        {
+            _model.TableName = tableName;
+            return this;    
+        }
+
+        public UpdateBuilder Set(string leftExpression)
+        {
+            _model.AddSetExpression(leftExpression, string.Empty);
+            return this;
+        }
+
+        public UpdateBuilder Set(string leftExpression, string rightExpression)
+        {
+            _model.AddSetExpression(leftExpression, rightExpression);
+            return this;
+        }
+
+        public UpdateBuilder Parameter(string parameterName, object value)
+        {
+            _model.AddParameter(parameterName, value);
+            return this;
+        }
+
+        public UpdateBuilder Parameter(string parameterName, object value, DbType dbType)
+        {
+            _model.AddParameter(parameterName, value, dbType);
+            return this;
+        }
+
+        public UpdateBuilder WhereAnd(string whereExpression)
+        {
+            _model.WhereAnd(whereExpression);
+
+            return this;
+        }
+
+        public UpdateBuilder WhereOr(string whereExpression)
+        {
+            _model.WhereAnd(whereExpression);
+
+            return this;
+        }
+
+        public static UpdateBuilder Make() { 
+
+            return new UpdateBuilder();
+        }
+
+    }
+}
