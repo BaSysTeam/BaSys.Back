857e7b142e2025bc5221a270cbda206aa931fcf4
ZyuDev <MikhaZyu@gmail.com>
2024-04-01 13:55:44 +0300

    #072-feature: Update script builder

---
+++ src/BaSys.FluentQueries/ScriptGenerators/UpdateScriptGenerator.cs
@@ -0,0 +1,28 @@
+﻿using BaSys.FluentQueries.Abstractions;
+using BaSys.FluentQueries.Enums;
+using BaSys.FluentQueries.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace BaSys.FluentQueries.ScriptGenerators
+{
+    internal class UpdateScriptGenerator: ScriptGeneratorBase
+    {
+        private readonly UpdateModel _model;
+
+        public UpdateScriptGenerator(UpdateModel model, SqlDialectKinds sqlDialect):base(sqlDialect)
+        {
+            _model = model;
+        }
+
+        public IQuery Build()
+        {
+            var query = new Query();
+
+            return query;
+        }
+
+    }
+}
