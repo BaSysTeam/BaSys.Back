3cf47f97432d7a7027d7a644a2e6ea0c3f1ac8f1
ZyuDev <MikhaZyu@gmail.com>
2024-04-02 13:51:23 +0300

    Model for application constants

---
+++ src/BaSys.Shared/Models/AppConstantsRecord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaSys.Common.Models
+{
+    public sealed class AppConstantsRecord
+    {
+        public Guid Uid { get; set; }
+        public Guid BaseUid { get; set; }
+        public string ApplicationTitle { get; set; }
+    }
+}
