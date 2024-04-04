4530d2e115931f612aac61be862b9b9bd81993bc
ZyuDev <MikhaZyu@gmail.com>
2024-04-04 12:52:07 +0300

    #090-feature: MetadataGroupCreate table

--- test/BaSys.FluentQueries.IntegrationTests/CreateTableBuilderTests.cs
+++ test/BaSys.FluentQueries.IntegrationTests/CreateTableBuilderTests.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using BaSys.Common.Enums;
+using BaSys.Host.DAL;
+using BaSys.Host.DAL.TableManagers;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,16 +26,60 @@ namespace BaSys.FluentQueries.IntegrationTests
             _configuration = builder.Build();
         }
 
+        [TearDown]
+        public async Task Teardown()
+        {
+            string pgConnectionString = _configuration?.GetConnectionString("pg_test_base");
+            string msConnectionString = _configuration?.GetConnectionString("ms_test_base");
+
+            var factory = new BaSysConnectionFactory();
+
+            using (IDbConnection connection = factory.CreateConnection(pgConnectionString, DbKinds.PgSql))
+            {
+                var manager = new MetadataGroupManager(connection);
+                await manager.DropTableAsync();
+            }
+
+            using (IDbConnection connection = factory.CreateConnection(msConnectionString, DbKinds.MsSql))
+            {
+                var manager = new MetadataGroupManager(connection);
+                await manager.DropTableAsync();
+            }
+
+        }
+
         [TestCase("pg_test_base")]
         [TestCase("ms_test_base")]
         public void GetConnectionStrings(string connectionStringName)
         {
             // Retrieve the connection string
-            string connectionString = _configuration?.GetConnectionString("pg_test_base") ?? string.Empty;
+            string connectionString = _configuration?.GetConnectionString(connectionStringName) ?? string.Empty;
 
             Console.WriteLine($"{connectionStringName}:{connectionString}");
 
             Assert.IsNotNull(connectionString);
+        }
+
+        [TestCase("pg_test_base", DbKinds.PgSql)]
+        [TestCase("ms_test_base", DbKinds.MsSql)]
+        public async Task CreateTable_MetadataGroup(string connectionStringName, DbKinds dbKind)
+        {
+
+            var tableExists = false;
+
+            var factory = new BaSysConnectionFactory();
+
+            string connectionString = _configuration?.GetConnectionString(connectionStringName) ?? string.Empty;
+
+            using(IDbConnection connection = factory.CreateConnection(connectionString, dbKind))
+            {
+                var manager = new MetadataGroupManager(connection);
+                await manager.CreateTableAsync();
+
+                tableExists = await manager.TableExistsAsync();
+            }
+
+            Assert.IsTrue(tableExists); 
         }
     }
 }
