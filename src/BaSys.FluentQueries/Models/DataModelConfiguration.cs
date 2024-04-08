8f00bdf62ba9a29c7fb66e73b006a8067a27b8b3
ZyuDev <MikhaZyu@gmail.com>
2024-04-08 12:36:16 +0300

    #094-feature: fill datamodel configuration from reflection

---
+++ src/BaSys.FluentQueries/Models/DataModelConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace BaSys.FluentQueries.Models
+{
+    public abstract class DataModelConfiguration<T> where T : class
+    {
+        private readonly List<TableColumn> _columns = new List<TableColumn>();
+
+        public string TableName { get; set; }
+        public IReadOnlyCollection<TableColumn> Columns => _columns;
+
+        public DataModelConfiguration()
+        {
+            InitTableName();
+        }
+
+        public TableColumn Column(string name)
+        {
+            return _columns.FirstOrDefault(x=>x.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private void InitTableName()
+        {
+            TableName = typeof(T).Name; 
+        }
+
+        private void InitColumns()
+        {
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                .Where(p => p.GetSetMethod() != null); // Only properties with public setters
+
+            foreach (var property in properties)
+            {
+                _columns.Add(new TableColumn(property.Name, property.PropertyType));
+            }
+        }
+    }
+}
--- src/BaSys.FluentQueries/Models/TableColumn.cs
+++ src/BaSys.FluentQueries/Models/TableColumn.cs
@@ -14,5 +14,65 @@ namespace BaSys.FluentQueries.Models
         public bool PrimaryKey { get; set; }
         public bool Required { get; set; }
         public bool Unique { get; set; }
+
+        public TableColumn()
+        {
+            
+        }
+
+        public TableColumn(string name, Type type)
+        {
+            Name = name.ToLower();
+            DbType = ToDbType(type);
+            Required = RequiredFromType(type);
+        }
+
+        private DbType ToDbType(Type type)
+        {
+            if (type == typeof(byte)) return DbType.Byte;
+            else if (type == typeof(sbyte)) return DbType.SByte;
+            else if (type == typeof(short)) return DbType.Int16;
+            else if (type == typeof(ushort)) return DbType.UInt16;
+            else if (type == typeof(int)) return DbType.Int32;
+            else if (type == typeof(uint)) return DbType.UInt32;
+            else if (type == typeof(long)) return DbType.Int64;
+            else if (type == typeof(ulong)) return DbType.UInt64;
+            else if (type == typeof(float)) return DbType.Single;
+            else if (type == typeof(double)) return DbType.Double;
+            else if (type == typeof(decimal)) return DbType.Decimal;
+            else if (type == typeof(bool)) return DbType.Boolean;
+            else if (type == typeof(string)) return DbType.String;
+            else if (type == typeof(char)) return DbType.StringFixedLength; // DbType.Char does not exist, mapping to closest equivalent
+            else if (type == typeof(Guid)) return DbType.Guid;
+            else if (type == typeof(DateTime)) return DbType.DateTime;
+            else if (type == typeof(DateTimeOffset)) return DbType.DateTimeOffset;
+            else if (type == typeof(byte[])) return DbType.Binary;
+            else if (type == typeof(byte?)) return DbType.Byte;
+            else if (type == typeof(sbyte?)) return DbType.SByte;
+            else if (type == typeof(short?)) return DbType.Int16;
+            else if (type == typeof(ushort?)) return DbType.UInt16;
+            else if (type == typeof(int?)) return DbType.Int32;
+            else if (type == typeof(uint?)) return DbType.UInt32;
+            else if (type == typeof(long?)) return DbType.Int64;
+            else if (type == typeof(ulong?)) return DbType.UInt64;
+            else if (type == typeof(float?)) return DbType.Single;
+            else if (type == typeof(double?)) return DbType.Double;
+            else if (type == typeof(decimal?)) return DbType.Decimal;
+            else if (type == typeof(bool?)) return DbType.Boolean;
+            else if (type == typeof(char?)) return DbType.StringFixedLength; // DbType.Char does not exist, mapping to closest equivalent
+            else if (type == typeof(Guid?)) return DbType.Guid;
+            else if (type == typeof(DateTime?)) return DbType.DateTime;
+            else if (type == typeof(DateTimeOffset?)) return DbType.DateTimeOffset;
+            else throw new ArgumentException("Unsupported type");
+        }
+
+        private bool RequiredFromType(Type type)
+        {
+            var underlyingType = Nullable.GetUnderlyingType(type);
+
+            return underlyingType == null;
+           
+        }
+
     }
 }
