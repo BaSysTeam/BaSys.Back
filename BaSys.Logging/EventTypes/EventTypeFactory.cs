81f2965a532d741dad4995d979da0aea7b852941
ZyuDev <MikhaZyu@gmail.com>
2024-04-25 16:34:00 +0300

    #117-feature: Add logger to metadata kinds.

---
+++ BaSys.Logging/EventTypes/EventTypeFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaSys.Logging.EventTypes
+{
+    public static class EventTypeFactory
+    {
+        public static MetadataCreateEventType MetadataCreate => new MetadataCreateEventType();
+        public static MetadataUpdateEventType MetadataUpdate => new MetadataUpdateEventType();  
+        public static MetadataDeleteEventType MetadataDelete => new MetadataDeleteEventType();  
+        public static SettingsChangedEventType SettingsChanged => new SettingsChangedEventType();   
+        public static UserLoginEventType UserLogin => new UserLoginEventType();
+
+    }
+}
--- src/BaSys.Logging.Infrastructure/EventType.cs
+++ src/BaSys.Logging.Infrastructure/EventType.cs
@@ -5,4 +5,9 @@ public abstract class EventType
     public abstract Guid Uid { get; }
     public abstract string EventName { get; }
     public virtual string Module => "core";
+
+    public override string ToString()
+    {
+        return EventName;
+    }
 }
