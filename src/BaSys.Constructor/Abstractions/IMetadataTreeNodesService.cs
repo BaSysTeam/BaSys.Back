1b5d9acf2d874e608d059b553188074f0a6eecfe
MAXIM18\18kmv <18kmv@80gmail.com>
2024-05-02 15:15:28 +0300

    #149-feature: added removing metaObject when remove tree node

--- src/BaSys.Constructor/Abstractions/IMetadataTreeNodesService.cs
+++ src/BaSys.Constructor/Abstractions/IMetadataTreeNodesService.cs
@@ -9,7 +9,7 @@ namespace BaSys.Constructor.Abstractions
         Task<ResultWrapper<List<MetadataTreeNodeDto>>> GetStandardNodesAsync();
         Task<ResultWrapper<int>> InsertAsync(MetadataTreeNodeDto dto);
         Task<ResultWrapper<int>> InsertMetaObjectAsync(CreateMetaObjectDto dto);
-        Task<ResultWrapper<int>> DeleteAsync(Guid uid);
+        Task<ResultWrapper<int>> DeleteAsync(MetadataTreeNodeDto dto);
         Task<ResultWrapper<List<MetadataTreeNodeDto>>> GetChildrenAsync(Guid uid);
     }
 }
