ef94ac4d90e0e1b6440bcd4623764d8196ff6a9c
Oceanshiver <62504973+Oceanshiver@users.noreply.github.com>
2024-05-06 16:49:03 +0300

    public api test example

---
+++ src/BaSys.PublicAPI/Controllers/PublicApiTestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaSys.PublicAPI.Controllers;
+
+[Route("api/public/v1/[controller]")]
+[ApiController]
+public class PublicApiTestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Test()
+    {
+        return Ok("ok");
+    }
+}
