91bb64d1fb8f02d1ea94e1f378200f26cbdf4f98
ZyuDev <MikhaZyu@gmail.com>
2024-05-07 13:03:42 +0300

    Model for FileStorage config

---
+++ src/BaSys.DAL.Models/Admin/FileStorageConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaSys.DAL.Models.Admin
+{
+    public sealed class FileStorageConfig
+    {
+        public Guid Uid { get; set; } = Guid.NewGuid();
+        public string S3ConnectionString { get; set; } = string.Empty;
+        public int MaxFileSizeMb { get; set; } = 50;
+        public bool IsEnabled { get; set; }
+    }
+}
