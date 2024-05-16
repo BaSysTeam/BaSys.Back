4866e1db2d498ee42770ba0e4d9a8696747fb137
ZyuDev <MikhaZyu@gmail.com>
2024-05-16 15:09:57 +0300

    #160-feature: Add column query in AlterTableBuilder

--- src/BaSys.FluentQueries/ScriptGenerators/AlterTableScriptGeneratorBase.cs
+++ src/BaSys.FluentQueries/ScriptGenerators/AlterTableScriptGeneratorBase.cs
@@ -3,6 +3,7 @@ using BaSys.FluentQueries.Enums;
 using BaSys.FluentQueries.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace BaSys.FluentQueries.ScriptGenerators
@@ -16,6 +17,8 @@ namespace BaSys.FluentQueries.ScriptGenerators
             _model = model;
         }
 
+        protected abstract string GetDataType(DbType dbType, int stringLenght);
+
         public virtual IQuery Build()
         {
             _model.ToLower();
@@ -26,9 +29,57 @@ namespace BaSys.FluentQueries.ScriptGenerators
             AppendName(_model.TableName);
             _sb.AppendLine();
 
+            var n = 1;
+            foreach (var column in _model.NewColumns)
+            {
+                if (n > 1)
+                    _sb.AppendLine(",");
+
+                AddColumnQuery(column, n);
+
+                n++;
+            }
+            _sb.Append(';');
+
             query.Text = _sb.ToString(); 
 
             return query;
+        }
+
+        private void AddColumnQuery(TableColumn column, int counter)
+        {
+            switch (_sqlDialect)
+            {
+                case SqlDialectKinds.MsSql:
+                    if (counter == 1)
+                      _sb.Append("ADD ");
+
+                    break;
+                case SqlDialectKinds.PgSql:
+                    _sb.Append("ADD COLUMN ");
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            AppendName(column.Name);
+            _sb.Append(' ');
+            _sb.Append(GetDataType(column.DbType, column.StringLength));
+
+            if (column.Required)
+            {
+                _sb.Append(" NOT NULL");
+            }
+            else
+            {
+                _sb.Append(" NULL");
+            }
+
+            if (column.Unique)
+            {
+                _sb.Append(" UNIQUE");
+            }
+
         }
     }
 }
---
+++ test/BaSys.FluentQueries.UnitTests/AlterTableBuilderTests.cs
@@ -0,0 +1,51 @@
+﻿using BaSys.FluentQueries.Enums;
+using BaSys.FluentQueries.Models;
+using BaSys.FluentQueries.QueryBuilders;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaSys.FluentQueries.UnitTests
+{
+    [TestFixture]
+    public class AlterTableBuilderTests
+    {
+        [TestCase(SqlDialectKinds.MsSql, "AlterTableAddColumnsMsSQl")]
+        [TestCase(SqlDialectKinds.PgSql, "AlterTableAddColumnsPgSQl")]
+        public void AlterTable_AddColumns_Query(SqlDialectKinds dialectKind, string checkKey)
+        {
+            var codeColumn = new TableColumn()
+            {
+                Name = "code",
+                DbType = DbType.String,
+                StringLength = 3,
+                Required = true,
+            };
+
+            var titleColumn = new TableColumn()
+            {
+                Name = "title",
+                DbType = DbType.String,
+                StringLength = 100,
+                Required = true,
+            };
+
+            var builder = AlterTableBuilder.Make()
+                .Table("cat_currency")
+                .AddColumn(codeColumn)
+                .AddColumn(titleColumn);
+
+            var query = builder.Query(dialectKind);
+
+            Console.WriteLine(dialectKind);
+            Console.WriteLine(query);
+            Console.WriteLine("==============");
+
+            Assert.Pass();
+
+        }
+    }
+}
