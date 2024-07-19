0d78cf7e416e87d105651ec542727c0de2dddd7a
ZyuDev <MikhaZyu@gmail.com>
2024-07-19 11:59:42 +0300

    #197-feature: DataObject DetailTable Manager.

---
+++ src/BaSys.Host.DAL/ModelConfigurations/DataObjectDetailTableConfiguration.cs
@@ -0,0 +1,60 @@
+﻿using BaSys.DAL.Models.App;
+using BaSys.FluentQueries.Models;
+using BaSys.Metadata.Abstractions;
+using BaSys.Metadata.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaSys.Host.DAL.ModelConfigurations
+{
+    public sealed class DataObjectDetailTableConfiguration: DataModelConfiguration<DataObjectDetailTable>
+    {
+        private readonly MetaObjectKindSettings _kindSettings;
+        private readonly MetaObjectStorableSettings _objectSettings;
+        private readonly MetaObjectTable _tableSettings;
+        private readonly IDataTypesIndex _dataTypesIndex;
+
+        public DataObjectDetailTableConfiguration(MetaObjectKindSettings kindSettings,
+            MetaObjectStorableSettings objectSettings,
+            MetaObjectTable tableSettings,
+            IDataTypesIndex dataTypesIndex): base(false)
+        {
+            _kindSettings = kindSettings;
+            _objectSettings = objectSettings;
+            _tableSettings = tableSettings;
+            _dataTypesIndex = dataTypesIndex;
+
+            Table(ComposeTableName(_kindSettings.Name, _objectSettings.Name, _tableSettings.Name));
+            AddColumns();
+        }
+
+        public static string ComposeTableName(string kindPrefix, string objectName, string tableName)
+        {
+            return $"{kindPrefix}_{objectName}_dt_{tableName}";
+        }
+
+        private void AddColumns()
+        {
+            // Add other columns.
+            foreach (var columnSettings in _tableSettings.Columns)
+            {
+                var tableColumn = new TableColumn()
+                {
+                    Name = columnSettings.Name,
+                    PrimaryKey = columnSettings.PrimaryKey,
+                    DbType = _dataTypesIndex.GetDbType(columnSettings.DataTypeUid),
+                    StringLength = columnSettings.StringLength,
+                    NumberDigits = columnSettings.NumberDigits,
+                    Required = columnSettings.Required,
+                    Unique = columnSettings.Unique,
+                };
+
+                AddColumn(tableColumn);
+
+            }
+        }
+    }
+}
---
+++ src/BaSys.Host.DAL/TableManagers/DataObjectDetailTableManager.cs
@@ -0,0 +1,45 @@
+﻿using BaSys.FluentQueries.Models;
+using BaSys.FluentQueries.QueryBuilders;
+using BaSys.Host.DAL.Abstractions;
+using BaSys.Host.DAL.ModelConfigurations;
+using BaSys.Metadata.Abstractions;
+using BaSys.Metadata.Helpers;
+using BaSys.Metadata.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaSys.Host.DAL.TableManagers
+{
+    public sealed class DataObjectDetailTableManager : TableManagerBase
+    {
+        public DataObjectDetailTableManager(IDbConnection connection,
+            MetaObjectKindSettings kindSettings,
+            MetaObjectStorableSettings objectSettings,
+            MetaObjectTable tableSettings,
+            IDataTypesIndex dataTypeIndex) :
+
+            base(connection,
+                new DataObjectDetailTableConfiguration(kindSettings,
+                    objectSettings,
+                    tableSettings,
+                    dataTypeIndex))
+        {
+
+        }
+
+        public async Task<int> AlterTableAsync(AlterTableModel model, IDbTransaction? transaction = null)
+        {
+            var builder = new AlterTableBuilder(model).Table(_tableName);
+            _query = builder.Query(_sqlDialectKind);
+
+            var result = await _connection.ExecuteAsync(_query.Text, null, transaction);
+
+            return result;
+        }
+    }
+}
