6bcf3d3a970c9bf14f9b1f1a99348414f61da6da
ZyuDev <mikhazyu@gmail.com>
2024-11-16 04:34:01 +0300

    #221-feature: Command parameters.

--- src/BaSys.Metadata/Models/MetaObjectCommand.cs
+++ src/BaSys.Metadata/Models/MetaObjectCommand.cs
@@ -1,5 +1,7 @@
-﻿using MessagePack;
+﻿using BaSys.Common.Enums;
+using MessagePack;
 using System;
+using System.Collections.Generic;
 
 namespace BaSys.Metadata.Models
 {
@@ -8,22 +10,31 @@ namespace BaSys.Metadata.Models
     {
         public Guid Uid { get; set; } = Guid.NewGuid();
         public Guid TableUid { get; set; }
+        public MetaObjectCommandKinds Kind { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Expression { get; set; } = string.Empty;
         public string Memo { get; set; } = string.Empty;
         public bool IsActive { get; set; } = true;
 
+        public List<MetaObjectCommandParameter> Parameters { get; set; } = new List<MetaObjectCommandParameter>();
+
         public MetaObjectCommand Clone()
         {
             var clone = new MetaObjectCommand();
             clone.Uid = Uid;
             clone.TableUid = TableUid;
+            clone.Kind = Kind;
             clone.Title = Title;
             clone.Name = Name;
             clone.Expression = Expression;
             clone.Memo = Memo;
             clone.IsActive = IsActive;
+
+            foreach (var param in Parameters) { 
+                clone.Parameters.Add(param.Clone());
+            }
+
 
             return clone;
         }
---
+++ src/BaSys.Metadata/Models/MetaObjectCommandParameter.cs
@@ -0,0 +1,35 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaSys.Metadata.Models
+{
+    [MessagePackObject(keyAsPropertyName: true)]
+    public sealed class MetaObjectCommandParameter
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public DbType DbType { get; set; } = DbType.String;
+
+        public MetaObjectCommandParameter Clone()
+        {
+            var clone = new MetaObjectCommandParameter()
+            {
+                Name = Name,
+                Value = Value,
+                DbType = DbType
+            };
+
+            return clone;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}:{Value}";
+        }
+    }
+}
