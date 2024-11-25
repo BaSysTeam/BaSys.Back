ed4d978fc045daa4b416d362ef8810935f98fc7c
ZyuDev <mikhazyu@gmail.com>
2024-11-25 23:58:43 +0300

    #223-feature: MetaObjectRecordsSettings model.

---
+++ src/BaSys.Metadata/Models/MetaObjectRecordsSettingsColumn.cs
@@ -0,0 +1,32 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaSys.Metadata.Models
+{
+    [MessagePackObject(keyAsPropertyName: true)]
+    public sealed class MetaObjectRecordsSettingsColumn
+    {
+        public Guid DestinationColumnUid { get; set; }
+        public string Expression { get; set; } = string.Empty;
+
+        public MetaObjectRecordsSettingsColumn Clone()
+        {
+            var clone = new MetaObjectRecordsSettingsColumn()
+            {
+                DestinationColumnUid = DestinationColumnUid,
+                Expression = Expression
+            };
+
+            return clone;
+        }
+
+        public override string ToString()
+        {
+            return $"{DestinationColumnUid} -> {Expression}";
+        }
+    }
+}
---
+++ src/BaSys.Metadata/Models/MetaObjectRecordsSettingsRow.cs
@@ -0,0 +1,32 @@
+﻿using BaSys.Common.Enums;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+
+namespace BaSys.Metadata.Models
+{
+    [MessagePackObject(keyAsPropertyName: true)]
+    public sealed class MetaObjectRecordsSettingsRow
+    {
+        public Guid Uid { get; set; } = Guid.NewGuid();
+        public Guid SourceUid { get; set; }
+        public RegisterRecordDirections Direction { get; set; }
+        public List<MetaObjectRecordsSettingsColumn> Columns { get; set; } = new List<MetaObjectRecordsSettingsColumn>();
+
+        public MetaObjectRecordsSettingsRow Clone() { 
+
+            var clone = new MetaObjectRecordsSettingsRow();
+            clone.Uid = Uid;
+            clone.SourceUid = SourceUid;
+            clone.Direction = Direction;
+
+            foreach (var column in Columns) {
+                clone.Columns.Add(column.Clone());
+            }
+
+
+            return clone;
+
+        }
+    }
+}
