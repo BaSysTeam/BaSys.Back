d647474da9015bc07b430ec98a4c57525c3d3d2d
ZyuDev <mikhazyu@gmail.com>
2024-12-02 04:12:14 +0300

    #226-feature: InMemory Logger.

---
+++ src/BaSys.Logging/InMemory/InMemoryLogMessage.cs
@@ -0,0 +1,21 @@
+﻿using BaSys.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaSys.Logging.InMemory
+{
+    public sealed class InMemoryLogMessage
+    {
+        public DateTime Period { get; set; }
+        public EventTypeLevels Level { get; set; }
+        public string Text { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return $"{Period:dd.MM.yyyy hh:mm:ss.fff} [{Level}] {Text}";
+        }
+    }
+}
---
+++ src/BaSys.Logging/InMemory/InMemoryLogger.cs
@@ -0,0 +1,76 @@
+﻿using BaSys.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaSys.Logging.InMemory
+{
+    public sealed class InMemoryLogger
+    {
+        private List<InMemoryLogMessage> _messages = new List<InMemoryLogMessage>();
+        private EventTypeLevels _minimumLevel = EventTypeLevels.Trace;
+
+        public InMemoryLogger(EventTypeLevels level)
+        {
+            _minimumLevel = level;
+        }
+
+        public void Clear()
+        {
+            _messages.Clear();
+        }
+
+        public void SetLevel(EventTypeLevels level) { 
+            _minimumLevel = level;
+        }
+
+        public void Log(EventTypeLevels level, string messageTemplate, params object[] args)
+        {
+            if (!IsEnabled(level))
+            {
+                return;
+            }
+
+            var messageText = string.Format(messageTemplate, args);
+
+            _messages.Add(new InMemoryLogMessage() { Level = level, Text = messageText });
+        }
+
+        public void LogTrace(string messageTemplate, params object[] args)
+        {
+            Log(EventTypeLevels.Trace, messageTemplate, args);
+        }
+
+        public void LogDebug(string messageTemplate, params object[] args)
+        {
+            Log(EventTypeLevels.Debug, messageTemplate, args);
+        }
+
+        public void LogInfo(string messageTemplate, params object[] args)
+        {
+            Log(EventTypeLevels.Info, messageTemplate, args);
+        }
+
+        public void LogWarning(string messageTemplate, params object[] args)
+        {
+            Log(EventTypeLevels.Warning, messageTemplate, args);
+        }
+
+        public void LogError(string messageTemplate, params object[] args)
+        {
+            Log(EventTypeLevels.Error, messageTemplate, args);
+        }
+
+        public void LogCritical(string messageTemplate, params object[] args)
+        {
+            Log(EventTypeLevels.Critical, messageTemplate, args);
+        }
+
+        private bool IsEnabled(EventTypeLevels level)
+        {
+            return _minimumLevel >= level;
+        }
+    }
+}
