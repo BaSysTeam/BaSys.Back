4992565938e7ad82c76ee8c4575452094cb473b4
ZyuDev <mikhazyu@gmail.com>
2024-12-09 10:47:24 +0300

    #226-feature: Get model endpoint.

--- src/BaSys.App/Services/DataObjectRecordsService.cs
+++ src/BaSys.App/Services/DataObjectRecordsService.cs
@@ -3,7 +3,9 @@ using BaSys.App.Models.DataObjectRecordsDialog;
 using BaSys.Common.Infrastructure;
 using BaSys.Host.DAL.Abstractions;
 using BaSys.Host.DAL.DataProviders;
+using BaSys.Translation;
 using System.Data;
+using System.Security.AccessControl;
 
 namespace BaSys.App.Services
 {
@@ -24,9 +26,82 @@ namespace BaSys.App.Services
             _kindProvider = _providerFactory.Create<MetaObjectKindsProvider>();
         }
 
-        public Task<ResultWrapper<DataObjectRecordsDialogViewModel>> GetModelAsync(string kind, string name, string uid)
+        public async Task<ResultWrapper<DataObjectRecordsDialogViewModel>> GetModelAsync(string kind, string name, string uid)
         {
-            throw new NotImplementedException();
+            var result = new ResultWrapper<DataObjectRecordsDialogViewModel>();
+
+            try
+            {
+               result = await ExecuteGetModelAsync(kind, name, uid);
+            }
+            catch(Exception ex)
+            {
+                result.Error(-1, $"Cannot get model. Message: {ex.Message}.", ex.StackTrace );
+            }
+
+            return result;
+        }
+
+        private async Task<ResultWrapper<DataObjectRecordsDialogViewModel>> ExecuteGetModelAsync(string kindName, string objectName, string uid)
+        {
+            var result = new ResultWrapper<DataObjectRecordsDialogViewModel>();
+
+            var allKinds = await _kindProvider.GetCollectionAsync(null);
+            var kind = allKinds.FirstOrDefault(x => x.Name.Equals(kindName, StringComparison.InvariantCultureIgnoreCase));
+
+            if (kind == null)
+            {
+                result.Error(-1, $"{DictMain.CannotFindMetaObjectKind}: {kindName}");
+                return result;
+            }
+
+            var objectKindSettings = kind.ToSettings();
+
+            var recordsDestinationKind = allKinds.FirstOrDefault(x => x.Uid == objectKindSettings.RecordsSettings.StorageMetaObjectKindUid);
+
+            if (recordsDestinationKind == null)
+            {
+                result.Error(-1, $"Cannot find records meta object kind by uid: {objectKindSettings.RecordsSettings.StorageMetaObjectKindUid}");
+                return result;
+            }
+
+            var metaObjectProvider = new MetaObjectStorableProvider(_connection, objectKindSettings.Name);
+            var metaObject = await metaObjectProvider.GetItemByNameAsync(objectName, null);
+
+            if (metaObject == null)
+            {
+                result.Error(-1, $"{DictMain.CannotFindMetaObject}: {kindName}.{objectName}");
+                return result;
+            }
+
+            var metaObjectSettings = metaObject.ToSettings();
+
+            var recordsMetaObjectProvider = new MetaObjectStorableProvider(_connection, recordsDestinationKind.Name);
+            
+
+            var model = new DataObjectRecordsDialogViewModel();
+
+            foreach(var recordsSettingsItem in metaObjectSettings.RecordsSettings)
+            {
+                var destinationMetaObject = await recordsMetaObjectProvider.GetItemAsync(recordsSettingsItem.DestinationMetaObjectUid, null);
+
+                if (destinationMetaObject == null)
+                {
+                    continue;
+                }
+
+                var tab = new DataObjectRecordsDialogTab()
+                {
+                    Uid = destinationMetaObject.Uid,
+                    Title = destinationMetaObject.Title
+                };
+
+                model.Tabs.Add(tab);
+            }
+
+            result.Success(model);
+
+            return result;
         }
 
         private void Dispose(bool disposing)
