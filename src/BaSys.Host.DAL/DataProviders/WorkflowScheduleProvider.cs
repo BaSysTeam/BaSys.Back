50d37ea737062f14e344666ccaf2acd0a6789858
ZyuDev <mikhazyu@gmail.com>
2025-02-06 19:03:42 +0300

    #248-feature: Backend for workflow schedule records.

---
+++ src/BaSys.Host.DAL/DataProviders/WorkflowScheduleProvider.cs
@@ -0,0 +1,66 @@
+﻿using BaSys.FluentQueries.QueryBuilders;
+using BaSys.Host.DAL.Abstractions;
+using BaSys.Host.DAL.ModelConfigurations;
+using BaSys.Metadata.Models.WorkflowModel;
+using Dapper;
+using System.Data;
+
+namespace BaSys.Host.DAL.DataProviders
+{
+    public sealed class WorkflowScheduleProvider : SystemObjectProviderBase<WorkflowScheduleRecord>
+    {
+        public WorkflowScheduleProvider(IDbConnection connection) : base(connection, new WorkflowScheduleRecordConfiguration())
+        {
+
+        }
+
+        public async Task<IEnumerable<WorkflowScheduleRecord>> GetCollectionAsync(Guid? workflowUid, bool? isActive, IDbTransaction? transaction)
+        {
+            var builder = SelectBuilder.Make()
+                .From(_config.TableName)
+                .Select("*");
+
+            if (isActive.HasValue)
+            {
+                builder.WhereAnd("isactive = @isActive")
+                    .Parameter("isActive", isActive.Value, DbType.Boolean);
+            }
+
+            if (workflowUid.HasValue)
+            {
+                builder.WhereAnd("workflowuid = @workflowUid")
+                    .Parameter("workflowUid", workflowUid.Value, DbType.Guid);
+            }
+
+            _query = builder.Query(_sqlDialect);
+
+            var result = await _dbConnection.QueryAsync<WorkflowScheduleRecord>(_query.Text, _query.DynamicParameters, transaction);
+
+            return result;
+        }
+
+        public override async Task<Guid> InsertAsync(WorkflowScheduleRecord item, IDbTransaction? transaction)
+        {
+            _query = InsertBuilder.Make(_config).FillValuesByColumnNames(true).Query(_sqlDialect);
+
+            item.BeforeSave();
+            var insertedCount = await _dbConnection.ExecuteAsync(_query.Text, item, transaction);
+
+            return InsertedUid(insertedCount, item.Uid);
+        }
+
+        public override async Task<int> UpdateAsync(WorkflowScheduleRecord item, IDbTransaction? transaction)
+        {
+            var result = 0;
+
+            _query = UpdateBuilder.Make(_config)
+              .WhereAnd("uid = @uid")
+              .Query(_sqlDialect);
+
+            item.BeforeSave();
+            result = await _dbConnection.ExecuteAsync(_query.Text, item, transaction);
+
+            return result;
+        }
+    }
+}
---
+++ src/BaSys.Workflows/Services/WorkflowsScheduleService.cs
@@ -0,0 +1,122 @@
+﻿using BaSys.Common.Infrastructure;
+using BaSys.Host.DAL.DataProviders;
+using BaSys.Metadata.Models.WorkflowModel;
+using BaSys.Translation;
+using BaSys.Workflows.Abstractions;
+using System.Data;
+
+namespace BaSys.Workflows.Services
+{
+    public sealed class WorkflowsScheduleService : IWorkflowsScheduleService
+    {
+        private IDbConnection? _connection;
+        private WorkflowScheduleProvider _provider;
+
+        public void SetUp(IDbConnection connection)
+        {
+            _connection = connection;
+            _provider = new WorkflowScheduleProvider(_connection);
+        }
+
+        public async Task<ResultWrapper<IEnumerable<WorkflowScheduleRecord>>> GetCollectionAsync(Guid? workflowUid, bool? isActive)
+        {
+            var result = new ResultWrapper<IEnumerable<WorkflowScheduleRecord>>();
+
+            try
+            {
+                var collection = await _provider.GetCollectionAsync(workflowUid, isActive, null);
+                result.Success(collection);
+            }
+            catch (Exception ex)
+            {
+                result.Error(-1, $"Cannot get data: {ex.Message}", ex.StackTrace);
+            }
+
+            return result;
+        }
+
+        public async Task<ResultWrapper<WorkflowScheduleRecord>> GetRecordAsync(Guid uid)
+        {
+            var result = new ResultWrapper<WorkflowScheduleRecord>();
+
+            try
+            {
+                var item = await _provider.GetItemAsync(uid, null);
+                if (item == null)
+                {
+                    result.Error(-1, $"{DictMain.CannotFindItem}: {uid}");
+                    return result;
+                }
+
+                result.Success(item);
+            }
+            catch (Exception ex)
+            {
+                result.Error(-1, $"Cannot get data: {ex.Message}", ex.StackTrace);
+            }
+
+            return result;
+        }
+
+        public async Task<ResultWrapper<Guid>> CreateAsync(WorkflowScheduleRecord record)
+        {
+            var result = new ResultWrapper<Guid>();
+
+            try
+            {
+                var newUid = await _provider.InsertAsync(record, null);
+                result.Success(newUid, DictMain.ItemSaved);
+            }
+            catch (Exception ex)
+            {
+                result.Error(-1, $"{DictMain.CannotCreateItem}: {ex.Message}", ex.StackTrace);
+
+            }
+
+            return result;
+        }
+
+        public async Task<ResultWrapper<int>> UpdateAsync(WorkflowScheduleRecord record)
+        {
+            var result = new ResultWrapper<int>();
+
+            try
+            {
+                var savedRecord = await _provider.GetItemAsync(record.Uid, null);
+                if (savedRecord == null)
+                {
+                    result.Error(-1, $"{DictMain.CannotFindItem} WorkflowScheduleRecord.{record.Uid}");
+                    return result;
+                }
+
+                savedRecord.CopyFrom(record);
+                var insertedCount = await _provider.UpdateAsync(savedRecord, null);
+                result.Success(insertedCount, DictMain.ItemSaved);
+            }
+            catch (Exception ex)
+            {
+                result.Error(-1, $"{DictMain.CannotUpdateItem}: {ex.Message}", ex.StackTrace);
+            }
+
+            return result;
+        }
+
+        public async Task<ResultWrapper<int>> DeleteAsync(Guid uid)
+        {
+            var result = new ResultWrapper<int>();
+
+            try
+            {
+                var deletedCount = await _provider.DeleteAsync(uid, null);
+                result.Success(deletedCount, DictMain.ItemDeleted);
+            }
+            catch (Exception ex)
+            {
+                result.Error(-1, $"{DictMain.CannotDeleteItem} WorkflowSheduleRecord.{uid}: {ex.Message}", ex.StackTrace);
+            }
+
+
+            return result;
+        }
+    }
+}
